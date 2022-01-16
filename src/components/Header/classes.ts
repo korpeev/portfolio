d8e94b3b5e6494087e725a121dfe4892e0977efb
Aslan <korpeevaslan@gmail.com>
2022-01-16 08:47:07 +0500

    fix menu

--- src/components/Header/classes.ts
+++ src/components/Header/classes.ts
@@ -2,13 +2,13 @@ import cn from 'classnames';
 export default {
   menuBlockClasses: (openMenu: boolean) =>
     cn(
-      'flex md:justify-center md:items-center md:absolute  md:top-14 md:bg-neutral-800 md:w-screen md:h-screen transition-transform duration-300 ease-in-out md:-translate-x-full md:right-0 md:mb-2',
+      'flex md:justify-center md:absolute  md:top-14 md:bg-neutral-800 md:w-screen md:h-screen transition-transform duration-300 ease-in-out md:-translate-x-full md:right-0 md:mb-2',
       {
         'md:translate-x-0': openMenu,
       }
     ),
   menuListClasses:
-    'md:flex-col md:items-center text-white uppercase font-semibold flex items-center space-x-4 md:space-x-0 md:space-y-4',
+    'md:flex-col md:items-center md:my-24 text-white uppercase font-semibold flex items-center space-x-4 md:space-x-0 md:space-y-4',
   menuListItemClasses:
     'hover:-rotate-3 transition duration-300 ease-linear cursor-pointer hover:text-gray-400 ',
   headerClasses: 'py-3 bg-neutral-800 sticky',
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import cn from 'classnames';
 import avatarLogo from '@/assets/Avatar-Maker.svg';
 const Main = () => {
+  const typewritterClasses = cn(
+    'block mt-5 font-semibold overflow-hidden border-r-4 border-black  whitespace-nowrap mx-auto tracking-widest animate-typing'
+  );
   return (
     <main id='main' className='pt-10'>
       <div className='container mx-auto px-5'>
@@ -9,9 +13,7 @@ const Main = () => {
           <div className='pt-2 text-center text-4xl'>
             <h1 className=''>Hi, I am Aslan</h1>
             <div className='w-full'>
-              <span className='block mt-5 font-semibold overflow-hidden border-r-4 border-black  whitespace-nowrap mx-auto tracking-widest animate-typing '>
-                Frontend Developer
-              </span>
+              <span className={typewritterClasses}>Frontend Developer</span>
             </div>
           </div>
         </div>
